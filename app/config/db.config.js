cdf3986e8b0e3b5be0ea1cd392892ee13c5ed1f1
Artem Medianyk <www206363@gmail.com>
2021-04-25 10:08:19 +0300

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -4,9 +4,3 @@ module.exports = {
   PASSWORD: "",
   DB: "sumish"
 };
-// module.exports = {
-//   HOST: "xlf3ljx3beaucz9x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-//   USER: "py7ei3yfi78l93im",
-//   PASSWORD: "ycr26sr02m7mhmwj",
-//   DB: "l79nyg6bxvz3wnls"
-// };
